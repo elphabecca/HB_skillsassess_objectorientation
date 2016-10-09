d0a36a8f63bb2b261e15c63c6679e8c5c464347f
Becca Saines <rebecca.saines@gmail.com>
2016-10-08 23:20:23 -0700

    First commit, put down a few notes for Section 1

---
+++ assessment.py
@@ -0,0 +1,41 @@
+"""
+Part 1: Discussion
+
+1. What are the three main design advantages that object orientation
+   can provide? Explain each concept.
+
+   Polymorphism
+   Encapsulation
+   Abstraction
+
+2. What is a class?
+
+    A class is...
+
+3. What is an instance attribute?
+
+    An instance (object) in a class, etc.
+
+4. What is a method?
+
+    A function within a class, etc.
+
+5. What is an instance in object orientation?
+
+    UNSURE
+
+6. How is a class attribute different than an instance attribute?
+   Give an example of when you might use each.
+
+   A class attribute is a detail or piece of information that is pre-determined
+   for the class.  ETC
+
+
+"""
+
+
+# Parts 2 through 5:
+# Create your classes and class methods
+# http://fellowship.hackbrightacademy.com/materials/skills/object-orientation/
+
+
